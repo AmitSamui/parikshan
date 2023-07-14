b3ace057055eaec18fdea7b5044de96f85b3aa90
Amit Samui <amitsamui@Amits-MacBook-Air.local>
2023-07-14 11:38:28 +0530

    contract deployed

--- truffle/contracts/SimpleStorage.sol
+++ truffle/contracts/SimpleStorage.sol
@@ -1,14 +1,139 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
 
-contract SimpleStorage {
-  uint256 value;
+pragma solidity >=0.5.0 <0.9.0;
 
-  function read() public view returns (uint256) {
-    return value;
-  }
+contract verifyCertificate {
+    event FileCertified(
+        address author,
+        string fileHash,
+        uint256 timestamp,
+        uint256 fileSize,
+        string fileExtension
+    );
 
-  function write(uint256 newValue) public {
-    value = newValue;
-  }
+    // contract deployer (application owner)
+    address owner;
+
+    // sturcture describing issuer
+    struct CertificateIssuerStructure {
+        address issuer_address;
+        string issuer_institution;
+        string issuer_name;
+    }
+
+    // structure describing candidate
+    struct CandidateStructure {
+        address candidate_address;
+        string candidate_name;
+    }
+
+    //structure describing file certificate
+    struct FileCertificate {
+        CertificateIssuerStructure issuer;
+        CandidateStructure candidate;
+        string fileHash;
+        string fileName;
+        uint256 timestamp;
+        uint256 fileSize;
+        string fileExtension;
+    }
+
+    //mapping the file certificates by hash
+    mapping(address => mapping(string => FileCertificate)) fileCertificatesMap;
+
+    // mapping user roles [admin , isssuer , candidate]
+    mapping(address => string) public user_roles;
+
+    constructor() {
+        user_roles[msg.sender] = "admin";
+        owner = msg.sender;
+    }
+
+    function addIssuer(address issuer_address) public {
+        // check if the person is owner
+        require(msg.sender == owner, "You can't add issuer");
+        // check if the issuer is already present
+        require(
+            keccak256(abi.encodePacked(user_roles[msg.sender])) !=
+                keccak256(abi.encodePacked("issuer")),
+            "issuer is already present"
+        );
+
+        user_roles[issuer_address] = "issuer";
+    }
+
+    function removeIssuer(address issuer_address) public {
+        // check if the person is owner
+        require(msg.sender == owner, "You can't add issuer");
+        // check if the issuer is already present
+        require(
+            keccak256(abi.encodePacked(user_roles[msg.sender])) ==
+                keccak256(abi.encodePacked("issuer")),
+            "The person is not issuer"
+        );
+
+        user_roles[issuer_address] = "user";
+    }
+
+    //function that allows issuer to certify a file
+    function certifyFile(
+        address candidate_address,
+        string memory candidate_name,
+        uint256 fileSize,
+        string memory issuer_institution,
+        string memory issuer_name,
+        string memory fileHash,
+        string memory fileName,
+        string memory fileExtension
+    ) public payable {
+        // check if the person is issuer
+        require(
+            keccak256(abi.encodePacked(user_roles[msg.sender])) ==
+                keccak256(abi.encodePacked("issuer")),
+            "You can't certify the certificate"
+        );
+
+        CandidateStructure memory candidate = CandidateStructure(
+            candidate_address,
+            candidate_name
+        );
+        CertificateIssuerStructure memory issuer = CertificateIssuerStructure(
+            msg.sender,
+            issuer_name,
+            issuer_institution
+        );
+        FileCertificate memory newFileCertificate = FileCertificate(
+            issuer,
+            candidate,
+            fileHash,
+            fileName,
+            block.timestamp,
+            fileSize,
+            fileExtension
+        );
+        fileCertificatesMap[candidate_address][fileHash] = newFileCertificate;
+        emit FileCertified(
+            msg.sender,
+            fileHash,
+            block.timestamp,
+            fileSize,
+            fileExtension
+        );
+    }
+
+    // verification of certificate
+    function verifyFile(
+        string memory fileHash,
+        address candidate_address
+    ) public view returns (FileCertificate memory, bool) {
+        FileCertificate memory fileCertificate = fileCertificatesMap[
+            candidate_address
+        ][fileHash];
+        // return's false of certificate is not verified else
+        bool exists = (bytes(
+            fileCertificatesMap[candidate_address][fileHash].fileHash
+        ).length != 0);
+        return (fileCertificate, exists);
+    }
 }
+
