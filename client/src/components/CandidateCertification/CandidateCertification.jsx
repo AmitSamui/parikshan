c650919fe9c321ab781b068ec053ed5cb1f8d101
Amit Samui <amitsamui@Amits-MacBook-Air.local>
2023-07-18 09:22:00 +0530

    controlled form

--- client/src/components/CandidateCertification/CandidateCertification.jsx
+++ client/src/components/CandidateCertification/CandidateCertification.jsx
@@ -1,31 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CandidateCertification.module.css";
 import UploadFile from "../UploadFile/UploadFile";
 
 const CandidateCertification = () => {
+  const [formValues, setFormValues] = useState({
+    issuerName: "",
+    issuerAddress: "",
+    candidateName: "",
+    certificateName: "",
+    candidateAddress: "",
+    ipfsHash: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmitEvent = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
   return (
     <div className={`${styles.candidateCertification}`}>
       <div className={`${styles.section}`}>
         <div className={`${styles.heading}`}>Candidate Certification Form</div>
-        <div className={`${styles.formContainer}`}>
+        <form
+          className={`${styles.formContainer}`}
+          onSubmit={handleSubmitEvent}
+        >
           <div className={`${styles.subFormContainer}`}>
             <div className={`${styles.subHeading}`}>Issuer Information</div>
             <div className={`${styles.subFormContainerTop}`}>
               <input
                 className={`${styles.formInput}`}
                 style={{
-                  marginRight: "1rem"
+                  marginRight: "1rem",
                 }}
+                onChange={handleChange}
+                required
                 name="issuerName"
                 type="text"
+                value={formValues.issuerName}
                 placeholder="Issuer Name"
               />
               <input
                 className={`${styles.formInput}`}
                 style={{
-                  marginLeft: "1rem"
+                  marginLeft: "1rem",
                 }}
+                onChange={handleChange}
+                required
                 name="issuerAddress"
+                value={formValues.issuerAddress}
                 type="text"
                 placeholder="Issuer Address"
               />
@@ -34,20 +62,24 @@ const CandidateCertification = () => {
           <div className={`${styles.subFormContainer}`}>
             <div className={`${styles.subHeading}`}>Candidate infomation</div>
             <div className={`${styles.subFormContainerTop}`}>
-            <input
+              <input
                 className={`${styles.formInput}`}
                 style={{
-                  marginRight: "1rem"
+                  marginRight: "1rem",
                 }}
-                name="candidateNamr"
+                onChange={handleChange}
+                value={formValues.candidateName}
+                name="candidateName"
                 type="text"
                 placeholder="Candidate Name"
               />
               <input
                 className={`${styles.formInput}`}
                 style={{
-                  marginLeft: "1rem"
+                  marginLeft: "1rem",
                 }}
+                onChange={handleChange}
+                value={formValues.certificateName}
                 name="certificateName"
                 type="text"
                 placeholder="Certificate Name"
@@ -56,6 +88,8 @@ const CandidateCertification = () => {
             <input
               className={`${styles.formInput}`}
               name="candidateAddress"
+              value={formValues.candidateAddress}
+              onChange={handleChange}
               type="text"
               placeholder="Candidate Address"
             />
@@ -63,10 +97,13 @@ const CandidateCertification = () => {
               className={`${styles.formInput}`}
               name="ipfsHash"
               type="text"
+              value={formValues.ipfsHash}
+              onChange={handleChange}
               placeholder="IPFS Hash"
             />
           </div>
-        </div>
+          <button type="submit">submit</button>
+        </form>
       </div>
       <div className={`${styles.uploadContainer}`}>
         <div className={`${styles.ruleSection}`}>
